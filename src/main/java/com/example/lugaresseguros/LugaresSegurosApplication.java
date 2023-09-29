3d1af9c4739562320903f4687c7ac57ae42babab
ElyCordova <elycordovamiranda@gmail.com>
2023-09-28 20:11:11 -0600

    create project

---
+++ src/main/java/com/example/lugaresseguros/LugaresSegurosApplication.java
@@ -0,0 +1,13 @@
+package com.example.lugaresseguros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LugaresSegurosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LugaresSegurosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lugaresseguros/LugaresSegurosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lugaresseguros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LugaresSegurosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
