6d135669bf261346eb592ecce2e6e0be1632b93b
ElyCordova <elycordovamiranda@gmail.com>
2023-10-05 19:00:02 -0600

    web server configurated and test endpoint created

---
+++ src/main/java/com/example/lugaresseguros/controllers/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.example.lugaresseguros.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @GetMapping("/") /*aquí le decimos cuál es la ruta*/
+    public String index(){
+        return "Hello World desde un endpoint!";
+    }
+}
